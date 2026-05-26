05c8da561a5a9e1cea1c7f3ddc3c438297092d14
Echo <github@3kh0.net>
2026-05-26 13:53:30 -0400

    fix brakeman flags

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -2,8 +2,9 @@ class RedirectsController < ApplicationController
   def show
     user = User.find_by(slack_id: params[:slack_id])
 
-    if user&.redirect_url.present?
-      redirect_to user.redirect_url, allow_other_host: true
+    url = user&.redirect_url
+    if url.present? && url.match?(/\Ahttps:\/\/[^\n]+\z/i)
+      redirect_to url, allow_other_host: true
     else
       @slack_id = params[:slack_id]
       render :not_found, status: :not_found
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :slack_id, presence: true, uniqueness: true
-  validates :redirect_url, format: { with: /\Ahttps:\/\//i, message: "must start with https://" }, allow_blank: true
+  validates :redirect_url, format: { with: /\Ahttps:\/\/[^\n]+\z/i, message: "must start with https://" }, allow_blank: true
 end
